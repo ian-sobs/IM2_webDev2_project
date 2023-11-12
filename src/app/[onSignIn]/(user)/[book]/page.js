a5acb559cf6b47aa442f3e184b5c1e16253103f0
Ian Sobs <ian.sobrecaray@gmail.com>
2023-11-13 01:23:39 +0800

    Changed the horizontal padding of some breakpoints

--- src/app/[onSignIn]/(user)/[book]/page.js
+++ src/app/[onSignIn]/(user)/[book]/page.js
@@ -4,9 +4,9 @@ export default function book({searchParams}){
     const sectionGridStyle = {
         allWidth: "min-h-screen bg-slate-100 pt-[25px] pb-[20px] flex flex-row justify-center",
         mobile:"px-9 ",
-        sm:"sm:px-16 ",
-        md:"md:px-20 ",
-        lg:"lg:px-24 ",
+        sm:"sm:px-3 ",
+        md:"md:px-4 ",
+        lg:"lg:px-20 ",
         alg: "alg:px-28",
         xl:"xl:px-32 "
     }
