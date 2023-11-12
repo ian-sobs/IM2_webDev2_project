0c1393f3d54c10b7ebe1986c120225b11ebb362a
Ian Sobs <ian.sobrecaray@gmail.com>
2023-11-12 21:36:22 +0800

    commented out component only import statements

--- src/components/bookPage/genInfo.js
+++ src/components/bookPage/genInfo.js
@@ -1,6 +1,6 @@
-'use client'
-import { useState, useEffect } from "react"
-import { useSearchParams } from 'next/navigation'
+// 'use client'
+// import { useState, useEffect } from "react"
+// import { useSearchParams } from 'next/navigation'
 import { Suspense } from 'react'
 import Image from 'next/image'
 
