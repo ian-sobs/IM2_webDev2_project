6e619cabc1b44428aac1ec883c735294e2ad27f4
Ian Sobs <ian.sobrecaray@gmail.com>
2023-11-12 21:37:47 +0800

    commendted out the import image statement in geninfoDesc.js

--- src/components/bookPage/genInfoDesc.js
+++ src/components/bookPage/genInfoDesc.js
@@ -1,7 +1,7 @@
 'use client'
 import { useState, useEffect } from "react"
 import { useSearchParams } from 'next/navigation'
-import Image from 'next/image'
+// import Image from 'next/image'
 
 // async function getBookId(){
 //     let searchParams = useSearchParams()
