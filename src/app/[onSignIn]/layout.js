f9f8ae5b23db0975f29c0c91a86a60a9343cae65
Ian Sobs <ian.sobrecaray@gmail.com>
2023-11-17 19:59:10 +0800

    Used Tab component of Headless UI in navbar.js

--- src/app/[onSignIn]/layout.js
+++ src/app/[onSignIn]/layout.js
@@ -22,7 +22,7 @@ export default function BookiiLayout({children}){
         },
     ]
     
-    
+    // 
     return (
         <div>
             
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,7 @@
 // The navbar for the pages in (user)
 'use client'
 import Link from 'next/link'
-
+import { Tab } from '@headlessui/react'
 
 export default function Navbar({fields, styles}){
     const tabStyle = "flex flex-wrap flex-row content-center  px-8 justify-center w-fit text-2xl hover:bg-[#e38a40] font-medium	"
@@ -10,9 +10,11 @@ export default function Navbar({fields, styles}){
         <>
             
                 <div className={styles + " flex flex-wrap flex-row"}>
-                    {fields.map((field, index)=><Link key={index} href={field.linkTo} className={tabStyle}><button >{field.name}</button></Link>)}
-                    
-
+                    <Tab.Group>
+                        <Tab.List className=" flex flex-row"> 
+                            {fields.map((field, index)=><Link key={index} href={field.linkTo} className="flex flex-row"><Tab className={tabStyle}>{field.name}</Tab></Link>)}
+                        </Tab.List>
+                    </Tab.Group>
                 </div>
             
             
