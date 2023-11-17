ea8238f973e65100a25df3a585a270e85dac282f
Ian Sobs <ian.sobrecaray@gmail.com>
2023-11-17 16:41:50 +0800

    Changed the favorite button style

---
+++ src/app/[onSignIn]/(user)/[book]/api/unFave/route.js
@@ -0,0 +1,19 @@
+import pool from "@/dbConn"
+
+export async function GET(request, {params}) {
+    const searchParams = request.nextUrl.searchParams
+    const userID = parseInt(searchParams.get('userID'))
+    const bookID = parseInt(searchParams.get('bookID'))
+
+    console.log("favoritesFirstRender", params)
+    console.log(`UserID:`, userID)
+    console.log(`BookID:`, bookID)
+
+    const poolPromise = pool.promise()
+    const conn = await poolPromise.getConnection()
+    const [rows, fields] = await conn.execute("DELETE FROM favorites f WHERE f.userID=? AND f.bookID=?", [userID, bookID])
+    const [result, resFields] = await conn.execute("SELECT f.favoriteID FROM favorites f WHERE f.userID=? AND f.bookID=?", [userID, bookID])
+    await poolPromise.releaseConnection(conn)
+
+    return Response.json(result)
+}
--- src/components/bookPage/faveButton.js
+++ src/components/bookPage/faveButton.js
@@ -39,7 +39,14 @@ export default function faveButton(props){
     }
 
     function unFave(){
-        setFaved(false)
+        fetch(`/user/book/api/unFave?userID=${props.userInfo.userID}&bookID=${props.bookInfo.bookID}`)
+        .then((result)=>result.json())
+        .then((parsedResult)=>{
+            if(parsedResult.length <= 0){
+                console.log("unFaved", parsedResult)
+                setFaved(false)
+            }
+        })
     }
 
     if(faved){
@@ -55,7 +62,7 @@ export default function faveButton(props){
 
     return (
         <>
-            <button className="bg-slate-200 py-[6px] px-[9px] rounded-full text-black hover:bg-red-400 fill-[#000000] hover:fill-white hover:text-white font-semibold flex flex-row items-center" onClick={fave}>
+            <button className="bg-slate-200 py-[6px] px-[9px] rounded-full text-red-400 hover:bg-red-400 fill-red-400 hover:fill-white hover:text-white font-semibold flex flex-row items-center" onClick={fave}>
                     {/* <Image className="mr-[4px] fill-[#000000] hover:fill-white" src={heart} alt="heartIcon"></Image> */}
                 <svg className="mr-[4px] " xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Z"/></svg>
                 Add to favorites
