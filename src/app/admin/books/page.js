2117a4c4e6182c5a88ffa97f2953634f29147af0
Ian Sobs <ian.sobrecaray@gmail.com>
2023-11-26 20:33:40 +0800

    Created a module export for the styles of the base admin page to reduce redundancy

--- src/app/admin/books/page.js
+++ src/app/admin/books/page.js
@@ -1,15 +1,17 @@
+import adminPageStyle from '../adminPageStyle'
+
 export default function books(){
-    const sectionGridStyle = {
-        allWidth: "min-h-screen pt-[25px] bg-slate-100 pb-[20px] grid place-items-center gap-6 w-full",
-        mobile:"grid-cols-2 px-9",
-        sm:"sm:px-11 sm:grid-cols-2",
-        md:"md:px-14 md:grid-cols-3",
-        lg:"lg:px-20 lg:grid-cols-4 ",
-        xl:"xl:px-22 xl:grid-cols-4"
-    }
+    // const adminPageStyle = {
+    //     allWidth: "min-h-screen pt-[25px] bg-slate-100 pb-[20px] w-full ",
+    //     mobile:" px-9 ",
+    //     sm:" sm:px-11 ",
+    //     md:" md:px-14 ",
+    //     lg:" lg:px-20 ",
+    //     xl:" xl:px-22 "
+    // }
     return(
         <>
-            <section name="sectionGrid" className={`${sectionGridStyle.allWidth} ${sectionGridStyle.mobile} ${sectionGridStyle.sm} ${sectionGridStyle.md} ${sectionGridStyle.lg} ${sectionGridStyle.xl}`}>
+            <section name="sectionGrid" className={`${adminPageStyle.allWidth} ${adminPageStyle.mobile} ${adminPageStyle.sm} ${adminPageStyle.md} ${adminPageStyle.lg} ${adminPageStyle.xl}`}>
             
                
             </section>
