64e354d4aeb3c0de85d17d132cd0fa7852ea151f
ian-sobs <ian.sobrecaray@gmail.com>
2023-11-29 12:47:41 +0800

    Edited the connection properties in dbConn.js to allow majority of group members to connect to the database easily. Any changes made to the local copy of dbConn.js will not also tracked by git

--- src/dbConn.js
+++ src/dbConn.js
@@ -4,8 +4,8 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     connectionLimit : 10,
     host            : '127.0.0.1',
-    user            : 'bookii',
-    password        : 'iikooB258#',
+    user            : 'root',
+    password        : '',
     database        : 'bookii'
   });
 
