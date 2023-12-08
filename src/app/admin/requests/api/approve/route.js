3b17ba7f19517c2a00810dcad53eb9daca10ad77
Ian Sobs <ian.sobrecaray@gmail.com>
2023-12-09 06:27:27 +0800

    jlaalfjaf

---
+++ src/app/admin/requests/api/approve/route.js
@@ -0,0 +1,20 @@
+import pool from "@/dbConn"
+export async function POST(request) {
+    const {rowID} = await request.json()
+    const poolPromise = pool.promise()
+    const conn = await poolPromise.getConnection()
+    const [result, fields] = await conn.execute("SELECT userID, bookID FROM shopping_cart WHERE userID=?", [rowID])
+    const [reqRoom] = result
+    console.log("reqRoom", reqRoom)
+
+    const [delRes, delResFields] = await conn.execute("DELETE FROM shopping_cart WHERE shoppingCartID=?", [rowID])
+    // const [destruct] = delRes
+    console.log("delRes", delRes)
+
+    if(destruct.affectedRows <= 0) return Response.json({approved: false})
+
+    const [insert, insertFields] = await conn.execute("INSERT INTO order (userID, bookID) VALUES (?, ?)", [reqRoom.userID, reqRoom.bookID])
+    console.log("insert", insert)
+    // if(insert.insertID <= 0) return Response.json({approved: false})
+    return Response.json({approved: true})
+  }
