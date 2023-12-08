8d047a7448b53ee8e62e5463b7afa636b085369c
Ian Sobs <ian.sobrecaray@gmail.com>
2023-12-08 23:22:56 +0800

    ladflaf

--- src/components/bookPage/purchaseBut.js
+++ src/components/bookPage/purchaseBut.js
@@ -1,12 +1,13 @@
 'use client'
 import Image from 'next/image'
 import cartAdd from '@/icons/addToCart.svg'
-import { useState } from 'react' 
+import { useState, useEffect } from 'react' 
 import * as Dialog from '@radix-ui/react-dialog'
 import * as Form from '@radix-ui/react-form'
 
 export default function purchase(props){
     const [isOpen, setIsOpen] = useState(false)
+    const [isRequest, setIsRequest] = useState(true)
     const [prodQuant, setProdQuant] = useState(1)
     let style = {
         filter: "invert(92%) sepia(100%) saturate(0%) hue-rotate(202deg) brightness(106%) contrast(106%)"
@@ -27,6 +28,10 @@ export default function purchase(props){
             animation: "contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1)",
             zIndex: "70"
     }
+    
+    useEffect(()=>{
+
+    }, [])
 
     function handleSubmit(e){
         e.preventDefault()
@@ -45,86 +50,112 @@ export default function purchase(props){
         .then((output)=> {
             console.log("purchaseButOutput", output)
             setIsOpen(false)
+            setIsRequest(false)
         })
     }
 
-    return (
-        <>
-            <Dialog.Root open={isOpen} modal={true}>
+    function handleCancel(e){
+        fetch("book/api/cancelDormRequest", {    
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)})
+        .then((result)=>result.json())
+        .then((output)=> {
+            console.log("purchaseButOutput", output)
+            setIsOpen(false)
+            setIsRequest(false)
+        })
+    }
 
-                <Dialog.Trigger asChild>
-                    <button className="bg-green-500 p-[4px] px-[9px] rounded-full text-white font-semibold flex flex-row justify-center items-center" onClick={()=>setIsOpen(true)}>
-                        {/* <Image src={cartAdd} className="mr-[4px]" style={style} alt="AddToCart"></Image> */}
-                        <div className="flex flex-row">
-                            {/* <span className="mr-[4px]">{"PHP "}</span>
-                            {`${props.bookInfo.priceUSD}`} */}
-                            Request
-                        </div>
-                    </button>
-                </Dialog.Trigger>  
-
-                <Dialog.Portal >
-                    <Dialog.Overlay className="fixed inset-0 bg-black opacity-75 z-40" style={{}}> </Dialog.Overlay>
-
-                    <Dialog.Content className="flex flex-col text-black" style={dialogContent}>
-                        <>
-                        <Dialog.Title className="text-center font-semibold mb-[10px] text-lg tracking-wide">
-                            Order details
-                        </Dialog.Title>
-
-                            
-                        <Form.Root className='flex flex-col justify-center mb-[8px]' onSubmit={handleSubmit}>
-                            {/* <Form.Field className='flex flex-row items-center mb-[8px]' name="prodQuant">
-                                    <Form.Label className='mr-[10px]'>Quantity</Form.Label>
-                                    <Form.Control className='w-[60px] bg-slate-200 p-[4px] rounded-sm' asChild><input type="number" min="1" value={prodQuant} onChange={(e)=>setProdQuant(e.target.value)}></input></Form.Control>
-                            </Form.Field> */}
-{/* 
-                            <div className='flex flex-row items-center mb-[14px]'>
-                                <p className='mr-[7px]'>Total price: </p> <span className='text-green-500 font-semibold'>{`PHP ${(props.bookInfo.priceUSD * prodQuant).toFixed(2)}`}</span>
-                            </div> */}
-
-                             <Form.Field className='flex flex-col items-center mb-[8px]' name="message">
-                                <div className="flex flex-row justify-between items-center">    
-                                    <Form.Label asChild className='mr-[10px] self-start text-left'>
-                                        <div className='w-full text-left'>
-                                            Message
-                                        </div>
-                                    </Form.Label>
-                                    <Form.Message className="text-sm font-light" match="valueMissing"> Address should not be empty </Form.Message>
-                                </div>
-                                <Form.Control className='w-full bg-slate-200 p-[4px] rounded-sm h-36' asChild><textarea type="text"></textarea></Form.Control>
-                            </Form.Field>
-{/* 
-                            <Form.Field name="totalPrice" asChild>
-                                <Form.Control asChild><input type="number" value={(props.bookInfo.priceUSD * prodQuant).toFixed(2)} hidden></input></Form.Control>
-                            </Form.Field> */}
-
-                            <Form.Field name="userID" asChild>
-                                <Form.Control asChild><input type="number" value={parseInt(props.userInfo.usr)} hidden></input></Form.Control>
-                            </Form.Field>
-
-                            <Form.Field name="bookID" asChild>
-                                <Form.Control asChild><input type="number" value={parseInt(props.bookInfo.bookID)} hidden></input></Form.Control>
-                            </Form.Field>
-
-                            <div className="flex flex-row justify-end mt-[8px]">
-                                <Dialog.Close className="bg-stone-300 p-[7px] rounded-md" onClick={()=>{
-                                    setIsOpen(false)
-                                    setProdQuant(1)
-                                }}> 
-                                    Cancel
-                                </Dialog.Close>
-                                <Form.Submit className="bg-green-500 p-[7px] text-white rounded-md ml-[15px]">
-                                    Add to cart
-                                </Form.Submit>
+    if(isRequest){
+        return (
+            <>
+                <Dialog.Root open={isOpen} modal={true}>
+    
+                    <Dialog.Trigger asChild>
+                        <button className="bg-green-500 p-[4px] px-[9px] rounded-full text-white font-semibold flex flex-row justify-center items-center" onClick={()=>setIsOpen(true)}>
+                            {/* <Image src={cartAdd} className="mr-[4px]" style={style} alt="AddToCart"></Image> */}
+                            <div className="flex flex-row">
+                                {/* <span className="mr-[4px]">{"PHP "}</span>
+                                {`${props.bookInfo.priceUSD}`} */}
+                                Request
                             </div>
+                        </button>
+                    </Dialog.Trigger>  
+    
+                    <Dialog.Portal >
+                        <Dialog.Overlay className="fixed inset-0 bg-black opacity-75 z-40" style={{}}> </Dialog.Overlay>
+    
+                        <Dialog.Content className="flex flex-col text-black" style={dialogContent}>
+                            <>
+                            <Dialog.Title className="text-center font-semibold mb-[10px] text-lg tracking-wide">
+                                Order details
+                            </Dialog.Title>
+    
+                                
+                            <Form.Root className='flex flex-col justify-center mb-[8px]' onSubmit={handleSubmit}>
+                                {/* <Form.Field className='flex flex-row items-center mb-[8px]' name="prodQuant">
+                                        <Form.Label className='mr-[10px]'>Quantity</Form.Label>
+                                        <Form.Control className='w-[60px] bg-slate-200 p-[4px] rounded-sm' asChild><input type="number" min="1" value={prodQuant} onChange={(e)=>setProdQuant(e.target.value)}></input></Form.Control>
+                                </Form.Field> */}
+    {/* 
+                                <div className='flex flex-row items-center mb-[14px]'>
+                                    <p className='mr-[7px]'>Total price: </p> <span className='text-green-500 font-semibold'>{`PHP ${(props.bookInfo.priceUSD * prodQuant).toFixed(2)}`}</span>
+                                </div> */}
+    
+                                 <Form.Field className='flex flex-col items-center mb-[8px]' name="message">
+                                    <div className="flex flex-row justify-between items-center">    
+                                        <Form.Label asChild className='mr-[10px] self-start text-left'>
+                                            <div className='w-full text-left'>
+                                                Message
+                                            </div>
+                                        </Form.Label>
+                                        <Form.Message className="text-sm font-light" match="valueMissing"> Address should not be empty </Form.Message>
+                                    </div>
+                                    <Form.Control className='w-full bg-slate-200 p-[4px] rounded-sm h-36' asChild><textarea type="text"></textarea></Form.Control>
+                                </Form.Field>
+    {/* 
+                                <Form.Field name="totalPrice" asChild>
+                                    <Form.Control asChild><input type="number" value={(props.bookInfo.priceUSD * prodQuant).toFixed(2)} hidden></input></Form.Control>
+                                </Form.Field> */}
+    
+                                <Form.Field name="userID" asChild>
+                                    <Form.Control asChild><input type="number" value={parseInt(props.userInfo.usr)} hidden></input></Form.Control>
+                                </Form.Field>
+    
+                                <Form.Field name="bookID" asChild>
+                                    <Form.Control asChild><input type="number" value={parseInt(props.bookInfo.bookID)} hidden></input></Form.Control>
+                                </Form.Field>
+    
+                                <div className="flex flex-row justify-end mt-[8px]">
+                                    <Dialog.Close className="bg-stone-300 p-[7px] rounded-md" onClick={()=>{
+                                        setIsOpen(false)
+                                        setProdQuant(1)
+                                    }}> 
+                                        Cancel
+                                    </Dialog.Close>
+                                    <Form.Submit className="bg-green-500 p-[7px] text-white rounded-md ml-[15px]">
+                                        Add to cart
+                                    </Form.Submit>
+                                </div>
+    
+                            </Form.Root>
+                            </>
+    
+                        </Dialog.Content>
+                    </Dialog.Portal>    
+                </Dialog.Root>
+            </>
+        )
+    }
 
-                        </Form.Root>
-                        </>
+    return (
+    <button className="bg-rose-600 p-[4px] px-[9px] rounded-full text-white font-semibold flex flex-row justify-center items-center" onClick={handleCancel}>
 
-                    </Dialog.Content>
-                </Dialog.Portal>    
-            </Dialog.Root>
-        </>
-    )
+        Cancel
+    </button>)
+    
 }
